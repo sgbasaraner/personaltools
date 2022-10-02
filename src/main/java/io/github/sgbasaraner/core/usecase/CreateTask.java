8236fabe02f751cc937b9fcbc5043af13e6b9184
sgbasaraner <sarpbasaraner@gmail.com>
2022-10-02 10:04:55 +0200

    add validations for create task

--- src/main/java/io/github/sgbasaraner/core/usecase/CreateTask.java
+++ src/main/java/io/github/sgbasaraner/core/usecase/CreateTask.java
@@ -1,6 +1,13 @@
 package io.github.sgbasaraner.core.usecase;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import javax.ws.rs.BadRequestException;
+
 import io.github.sgbasaraner.core.model.Task;
+import io.github.sgbasaraner.core.model.Task.Status;
 import io.github.sgbasaraner.core.repository.TaskRepository;
 
 public class CreateTask {
@@ -11,6 +18,18 @@ public class CreateTask {
     }
 
     public Task run(Task task) {
-        return repository.createTask(task);
+        return repository.createTask(validate(task));
+    }
+
+    private Task validate(Task task) {
+        if (task.title == null || task.title.isEmpty()) {
+            throw new BadRequestException("Task title can't be empty.");
+        }
+
+        if (task.deadline.isPresent() && task.deadline.get().isBefore(LocalDateTime.now())) {
+            throw new BadRequestException("Deadline must be in the future");
+        }
+
+        return new Task(UUID.randomUUID(), task.title, task.notes, task.deadline, Status.Ready);
     }
 }
